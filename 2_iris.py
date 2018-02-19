bba7e37b01c4128cf21b4b19501ac9aec4469249
zhaobe <zhaoben777@gmail.com>
2018-02-19 00:08:29 -0800

    Modified iris file to output trained data and create graph visualization pdf.

--- 2_iris.py
+++ 2_iris.py
@@ -1,16 +1,39 @@
+import numpy as np
 from sklearn.datasets import load_iris
+from sklearn import tree
 
-#load iris data
+# load iris data
 iris = load_iris()
+test_idx = [0, 50, 100]
 
-# column headers
-print iris.feature_names
-print iris.target_names
+# training data
+train_target = np.delete(iris.target, test_idx)
+train_data = np.delete(iris.data, test_idx, axis=0)
 
-# first row of data
-print iris.data[0]
-print iris.target[0]
+# testing data
+test_target = iris.target[test_idx]
+test_data = iris.data[test_idx]
 
-# load dataset in nice fashion
-for i in range(len(iris.target)):
-	print "Example %d: \tlabel %s, \tfeatures %s" % (i , iris.target[i], iris.data[i])
+clf = tree.DecisionTreeClassifier()
+clf.fit(train_data, train_target)
+
+# these printouts should match
+print test_target
+print clf.predict(test_data)
+
+# viz code to generate graphviz
+from sklearn.externals.six import StringIO
+import pydotplus
+dot_data = StringIO()
+tree.export_graphviz(clf, 
+						out_file=dot_data,
+						feature_names=iris.feature_names,
+						class_names=iris.target_names,
+						filled=True, rounded=True,
+						impurity=False)
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_pdf("2_iris.pdf")
+
+# printouts for 
+print test_data[1], test_target[1]
+print iris.feature_names, iris.target_names
--- README.md
+++ README.md
@@ -6,4 +6,5 @@ This project requires pip to be installed.
 - if you don't have the most recent pip, run `pip install --upgrade pip`
 - run `pip install --user numpy scipy` to install numpy and scipy
 - run `pip install -U scikit-learn` to install the scikit
-
+- run `pip install pydotplus graphviz` to install pydot and graphviz library 
+	- note: had to `brew install graphviz` due to error with graphviz
