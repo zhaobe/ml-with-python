de499cb4aac7d3799dd0f509c68a07c2ba66a1f8
zhaobe <zhaoben777@gmail.com>
2018-02-26 00:01:52 -0800

    Base code for 5_classifier is from previous 4_pipeline code.

--- 5_classifier.py
+++ 5_classifier.py
@@ -4,16 +4,11 @@ iris = datasets.load_iris()
 X = iris.data
 y = iris.target
 
-# sklearn.cross_validation will be deprecated so replaced with model_selection
+# sklearn.cross_validation will be deprecated so replaced with sklearn.model_selection
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .5)
 
-# from sklearn import tree
-# my_classifier = tree.DecisionTreeClassifier()
-
-# switched to using KNeighbor classifier instead of decision tree
-from sklearn.neighbors import KNeighborsClassifier
-my_classifier = KNeighborsClassifier()
+# TODO: new classifier here
 
 my_classifier.fit(X_train, y_train)
 
